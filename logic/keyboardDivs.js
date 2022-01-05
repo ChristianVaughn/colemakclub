0ac72158a60164420f32e2df8071a2e0b7f1a120
Christian Vaughn <me@christianvaughn.net>
2022-01-04 20:45:02 -0800

    Make cheatsheat look like ErgoTravel Light and change header name

--- logic/keyboardDivs.js
+++ logic/keyboardDivs.js
@@ -1,74 +1,76 @@
 var keyboardDivs = `
+
 	<div class=row>
-		<div class="key" id='Backquote'><span class="letter"></span></div>
-		<div class="key" id='Digit1'><span class="letter"></span></div>
-		<div class="key" id='Digit2'><span class="letter"></span></div>
-		<div class="key" id='Digit3'><span class="letter"></span></div>
-		<div class="key" id='Digit4'><span class="letter"></span></div>
-		<div class="key" id='Digit5'><span class="letter"></span></div>
-		<div class="key" id='Digit6'><span class="letter"></span></div>
-		<div class="key" id='Digit7'><span class="letter"></span></div>
-		<div class="key" id='Digit8'><span class="letter"></span></div>
-		<div class="key" id='Digit9'><span class="letter"></span></div>
-		<div class="key" id='Digit0'><span class="letter"></span></div>
-		<div class="key" id='Minus'><span class="letter"></span></div>
-		<div class="key" id='Equal'><span class="letter"></span></div>
-		<div class="key twou" id='BackSpace'><span class="letter"></span></div>
-	</div>
-	<div class=row>
-		<div class="key onepointfiveu"><span class="letter"></span></div>
-		<div class="key" id='KeyQ'><span class="letter"></span></div>
-		<div class="key" id='KeyW'><span class="letter"></span></div>
+		<div class="key down3"><span class="letter"></span></div>
+		<div class="key down2" id='KeyQ'><span class="letter"></span></div>
+		<div class="key down1" id='KeyW'><span class="letter"></span></div>
 		<div class="key" id='KeyE'><span class="letter"></span></div>
-		<div class="key lvl3" id='KeyR'><span class="letter"></span></div>
-		<div class="key lvl3" id='KeyT'><span class="letter"></span></div>
-		<div class="key lvl3" id='KeyY'><span class="letter"></span></div>
-		<div class="key lvl3" id='KeyU'><span class="letter"></span></div>
+		<div class="key lvl3 down1" id='KeyR'><span class="letter"></span></div>
+		<div class="key lvl3 down2" id='KeyT'><span class="letter"></span></div>
+		<div class="key down3" id='BracketLeft'><span class="letter"></span></div>
+		<div class="key invis"><span class="letter"></span></div>
+
+		<div class="key down3" id='BracketRight'><span class="letter"></span></div>
+		<div class="key lvl3 down2" id='KeyY'><span class="letter"></span></div>
+		<div class="key lvl3 down1" id='KeyU'><span class="letter"></span></div>
 		<div class="key lvl5" id='KeyI'><span class="letter"></span></div>
-		<div class="key lvl5" id='KeyO'><span class="letter"></span></div>
-		<div class="key lvl5" id='KeyP'><span class="letter"></span></div>
-		<div class="key" id='BracketLeft'><span class="letter"></span></div>
-		<div class="key" id='BracketRight'><span class="letter"></span></div>
-		<div class="key onepointfiveu" id='Backslash'><span class="letter"></span></div>
+		<div class="key lvl5 down1" id='KeyO'><span class="letter"></span></div>
+		<div class="key lvl5 down2" id='KeyP'><span class="letter"></span></div>
+		<div class="key down3" id='Backslash'><span class="letter"></span></div>
 	</div>
 	<div class=row>
-		<div class="key onepointsevenfiveu"><span class="letter"></span></div>
-		<div class="key lvl1" id='KeyA'><span class="letter"></span></div>
-		<div class="key lvl1" id='KeyS'><span class="letter"></span></div>
+		<div class="key down3"><span class="letter"></span></div>
+		<div class="key lvl1 down2" id='KeyA'><span class="letter"></span></div>
+		<div class="key lvl1 down1" id='KeyS'><span class="letter"></span></div>
 		<div class="key lvl1" id='KeyD'><span class="letter"></span></div>
-		<div class="key lvl1 restingPosition" id='KeyF'><span class="letter"></span></div>
-		<div class="key lvl2" id='KeyG'><span class="letter"></span></div>
-		<div class="key lvl2" id='KeyH'><span class="letter"></span></div>
-		<div class="key lvl1 restingPosition" id='KeyJ'><span class="letter"></span></div>
+		<div class="key lvl1 restingPosition down1" id='KeyF'><span class="letter"></span></div>
+		<div class="key lvl2 down2" id='KeyG'><span class="letter"></span></div>
+		<div class="key down3" id='BracketLeft'><span class="letter"></span></div>
+		<div class="key invis"><span class="letter"></span></div>
+
+		<div class="key down3" id='BracketRight'><span class="letter"></span></div>
+		<div class="key lvl2 down2" id='KeyH'><span class="letter"></span></div>
+		<div class="key lvl1 restingPosition down1" id='KeyJ'><span class="letter"></span></div>
 		<div class="key lvl1" id='KeyK'><span class="letter"></span></div>
-		<div class="key lvl1" id='KeyL'><span class="letter"></span></div>
-		<div class="key lvl1" id='Semicolon'><span class="letter"></span></div>
-		<div class="key" id='Quote'><span class="letter"></span></div>
-		<div class="key twopointtwofiveu"><span class="letter"></span></div>
+		<div class="key lvl1 down1" id='KeyL'><span class="letter"></span></div>
+		<div class="key lvl1 down2" id='Semicolon'><span class="letter"></span></div>
+		<div class="key down3" id='Quote'><span class="letter"></span></div>
 	</div>
 	<div class=row>
-		<div class="key twopointtwofiveu"><span class="letter"></span></div>
-		<div class="key" id='KeyZ'><span class="letter"></span></div>
-		<div class="key lvl6" id='KeyX'><span class="letter"></span></div>
+		<div class="key down3"><span class="letter"></span></div>
+		<div class="key down2" id='KeyZ'><span class="letter"></span></div>
+		<div class="key lvl6 down1" id='KeyX'><span class="letter"></span></div>
 		<div class="key lvl6" id='KeyC'><span class="letter"></span></div>
-		<div class="key lvl4" id='KeyV'><span class="letter"></span></div>
-		<div class="key lvl4" id='KeyB'><span class="letter"></span></div>
-		<div class="key lvl4" id='KeyN'><span class="letter"></span></div>
-		<div class="key lvl4" id='KeyM'><span class="letter"></span></div>
+		<div class="key lvl4 down1" id='KeyV'><span class="letter"></span></div>
+		<div class="key lvl4 down2" id='KeyB'><span class="letter"></span></div>
+		<div class="key down3 spaceOne"><span class="letter"></span></div>
+		<div class="key invis"><span class="letter"></span></div>
+		<div class="key down3 spaceThree"><span class="letter"></span></div>
+
+		<div class="key lvl4 down2" id='KeyN'><span class="letter"></span></div>
+		<div class="key lvl4 down1" id='KeyM'><span class="letter"></span></div>
 		<div class="key lvl5" id='Comma'><span class="letter"></span></div>
-		<div class="key lvl6" id='Period'><span class="letter"></span></div>
-		<div class="key lvl6" id='Slash'><span class="letter"></span></div>
-		<div class="key twopointsevenfiveu"><span class="letter"></span></div>
+		<div class="key lvl6 down1" id='Period'><span class="letter"></span></div>
+		<div class="key lvl6 down2" id='Slash'><span class="letter"></span></div>
+		<div class="key down3"><span class="letter"></span></div>
 	</div>
 	<div class=row>
-		<div class="key onepointtwofiveu"><span class="letter"></span></div>
-		<div class="key onepointtwofiveu"><span class="letter"></span></div>
-		<div class="key onepointtwofiveu"><span class="letter"></span></div>
-		<div class="key sixpointtwofiveu"><span class="letter"></span></div>
-		<div class="key onepointtwofiveu"><span class="letter"></span></div>
-		<div class="key onepointtwofiveu"><span class="letter"></span></div>
-		<div class="key onepointtwofiveu"><span class="letter"></span></div>
-		<div class="key onepointtwofiveu"><span class="letter"></span></div>
+		<div class="key down3"><span class="letter"></span></div>
+		<div class="key down2"><span class="letter"></span></div>
+		<div class="key down1"><span class="letter"></span></div>
+		<div class="key "><span class="letter"></span></div>
+		<div class="key modifierLeft down2 "><span class="letter"></span></div>
+		<div class="key invis"><span class="letter"></span></div>
+		<div class="key down3 spaceTwo"><span class="letter"></span></div>
+		<div class="key invis"><span class="letter"></span></div>
+
+		<div class="key down3 spaceFour"><span class="letter"></span></div>
+		<div class="key invis"><span class="letter"></span></div>
+		<div class="key modifierRight down2"><span class="letter"></span></div>
+		<div class="key"><span class="letter"></span></div>
+		<div class="key down1"><span class="letter"></span></div>
+		<div class="key down2"><span class="letter"></span></div>
+		<div class="key down3"><span class="letter"></span></div>
 	</div>
 `;
 
